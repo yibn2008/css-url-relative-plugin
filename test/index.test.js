cd10329f9381d12ecf342888d18fe405712edd1e
yibn2008 <yibn2008@qq.com>
2020-09-23 14:29:39 +0800

    fix: fix #2

--- test/index.test.js
+++ test/index.test.js
@@ -4,12 +4,13 @@ const fs = require('fs')
 const path = require('path')
 const assert = require('assert')
 const webpack = require('webpack')
+const cssReplace = require('../lib/css-replace')
 
 describe('test plugin', function () {
   this.timeout(60000)
+  const basedir = path.join(__dirname, 'fixtures/simple')
 
   it('should compile with simple config', function (done) {
-    const basedir = path.join(__dirname, 'fixtures/simple')
     const config = require(path.join(basedir, 'webpack.config.js'))
     webpack(config).run((err, stats) => {
       if (err) {
@@ -34,5 +35,14 @@ describe('test plugin', function () {
         }
       }
     })
+  })
+
+  it('should css replace @import correctly', function (done) {
+    const original = fs.readFileSync(path.join(basedir, 'src/page/index.css'), 'utf-8')
+    const replaced = cssReplace(original, refer => {
+      return refer.rule;
+    })
+    assert.strictEqual(original, replaced)
+    done()
   })
 })
